581bf70388a76111d99eea9b1f19484fcf4e53fa
EduardoBarreto <eduardobarreto61@gmail.com>
2017-10-31 23:00:09 -0200

    Update cores.py

--- cores.py
+++ cores.py
@@ -48,5 +48,75 @@ extrusion(path=paths.arc(radius=2,
                  width=0.6, height=0.4)],
              [shapes.rectangle(pos=[0,1.8],
                  width=1,height=0.3)] ])
+from vpython import *
+# This version uses VPython slider
+# See ButtonsSlidersMenus1 for a version that uses Jupyter slider
 
+scene.userzoom = True
+scene.userspin = True
+scene.width = 400
+scene.height = 200
+scene.range = 1
+box(pos=vector(10,0,0)) # Force creation of canvas; box is not seen because it is outside the canvas
+cancopy = 'You can Ctrl-C or Command-C copy these RGB and HSV values:\n'
+scene.title = cancopy
+scene.append_to_title("RGB = <1.000, 0.000, 0.000>, HSV = <0.000, 0.000, 0.000>")
+
+C = ['Red', 'Green', 'Blue', 'Hue', 'Saturation', 'Value']
+sliders = []
+
+def set_background(sl):
+    if sl.id < 3:
+        rgb = vector(sliders[0].value, sliders[1].value, sliders[2].value)
+        hsv = color.rgb_to_hsv(rgb)
+        sliders[3].value = int(1000*hsv.x)/1000# reset HSV slider positions; display 3 figures
+        sliders[4].value = int(1000*hsv.y)/1000
+        sliders[5].value = int(1000*hsv.z)/1000
+    else:
+        hsv = vector(sliders[3].value, sliders[4].value, sliders[5].value)
+        rgb = color.hsv_to_rgb(hsv)
+        sliders[0].value = int(1000*rgb.x)/1000 # reset RGB slider positions; display 3 figures
+        sliders[1].value = int(1000*rgb.y)/1000
+        sliders[2].value = int(1000*rgb.z)/1000
+    scene.background = rgb
+    # For readability, limit precision of display of quantities to 3 figures
+    f = "RGB = <{:1.3f}, {:1.3f}, {:1.3f}>, HSV = <{:1.3f}, {:1.3f}, {:1.3f}>"
+    scene.title = cancopy + f.format(rgb.x, rgb.y, rgb.z, hsv.x, hsv.y, hsv.z)
+
+scene.caption = '\n'
+for i in range(6): # Create the 3 RGB and 3 HSV sliders
+    sliders.append(slider(length=300, left=10, min=0, max=1, bind=set_background, id=i))
+    scene.append_to_caption('    '+C[i]+'\n\n') # Display slider name
+    if i == 2: scene.append_to_caption("\n\n") # Separate the RGB and HSV sliders
+sliders[0].value = 1 # make the background red
+sliders[4].value = sliders[5].value = 1
+
+extrusion(path=paths.arc(radius=2,
+    angle1=-pi/3, angle2=pi+pi/3), 
+    shape=[ [shapes.triangle(length=2),              shapes.circle(pos=[0,.5], radius=0.2),
+             shapes.trapezoid(pos=[0,-0.2],
+                 width=0.6, height=0.4)],
+             [shapes.rectangle(pos=[0,1.8],
+                 width=1,height=0.3)] ])
+         
+if sl.id < 3:
+        rgb = vector(sliders[0].value, sliders[1].value, sliders[2].value)
+        hsv = color.rgb_to_hsv(rgb)
+        sliders[3].value = int(1000*hsv.x)/1000# reset HSV slider positions; display 3 figures
+        sliders[4].value = int(1000*hsv.y)/1000
+        sliders[5].value = int(1000*hsv.z)/1000
+    else:
+        hsv = vector(sliders[3].value, sliders[4].value, sliders[5].value)
+        rgb = color.hsv_to_rgb(hsv)
+        sliders[0].value = int(1000*rgb.x)/1000 # reset RGB slider positions; display 3 figures
+        sliders[1].value = int(1000*rgb.y)/1000
+        sliders[2].value = int(1000*rgb.z)/1000
+    scene.path = rgb
+scene.caption = '\n'
+for i in range(6): # Create the 3 RGB and 3 HSV sliders
+    sliders.append(slider(length=300, left=10, min=0, max=1, bind=set_background, id=i))
+    scene.append_to_caption('    '+C[i]+'\n\n') # Display slider name
+    if i == 2: scene.append_to_caption("\n\n") # Separate the RGB and HSV sliders
+sliders[0].value = 1 # make the background red
+sliders[4].value = sliders[5].value = 1
 
